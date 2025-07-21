ac0bfdae008fd9f3de53babd6e72bb7e7cc017bc
AbderahmanKh <abderahmankhajaa0@gmail.com>
2025-07-21 15:34:50 +0100

    first project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+    Starting Samar's website with Vite and Tailwind CSS
+  </h1>
+    </>
+  )
+}
+
+export default App
